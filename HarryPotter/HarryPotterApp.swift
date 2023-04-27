db5751350413370411ae0e8c8a7c609458ec7522
Grace Gong <ggong@lcs.on.ca>
2023-04-27 19:37:58 -0400

    Initial Commit

---
+++ HarryPotter/HarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterApp.swift
+//  HarryPotter
+//
+//  Created by Grace Gong on 2023-04-27.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
