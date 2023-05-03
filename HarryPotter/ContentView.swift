4a2c025cfdc233b5eb8a92e774ae2b4f0f1bffc3
Grace Gong <ggong@lcs.on.ca>
2023-05-03 13:19:13 -0400

    finish the last step hair color

--- HarryPotter/ContentView.swift
+++ HarryPotter/ContentView.swift
@@ -10,12 +10,125 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Text("Harry Potter")
+                    .font(Font.system(size:45, weight: .bold))
+            
+                Spacer()
+                
+            }
+            
+            HStack{
+                Image("harry")
+                
+                Spacer()
+            }
+            
+            HStack{
+                Text("Also Known As...")
+                    .font(Font.system(size:20, weight: .regular))
+                Spacer()
+            }
+            
+            .padding()
+            
+            HStack{
+                Text("The Boy Who Lived")
+                    .font(Font.system(size:23, weight: .bold))
+                Spacer()
+            }
+           
+            HStack{
+                Text("The Chosen One")
+                    .font(Font.system(size:23, weight: .bold))
+                Spacer()
+            }
+            
+         Spacer()
+            
+            
+            Group{
+                
+                HStack{
+                    Text("Gender")
+                        .font(Font.system(size:20, weight: .regular))
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("male")
+                        .font(Font.system(size:22, weight: .regular))
+                    Spacer()
+                }
+                
+          Spacer()
+                
+                HStack{
+                    Text("House")
+                        .font(Font.system(size:20, weight: .regular))
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("Gryffindor")
+                        .font(Font.system(size:22, weight: .regular))
+                    Spacer()
+                }
+
+                Spacer()
+                
+                HStack{
+                    Text("Date of birth")
+                        .font(Font.system(size:20, weight: .regular))
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("31-07-1980")
+                        .font(Font.system(size:22, weight: .regular))
+                    Spacer()
+                }
+            
+                
+            }
+            
+            Group{
+                
+                Spacer()
+                
+                HStack{
+                    Text("Eye color")
+                        .font(Font.system(size:20, weight: .regular))
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("green")
+                        .font(Font.system(size:22, weight: .regular))
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Text("Hair color")
+                        .font(Font.system(size:20, weight: .regular))
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("black")
+                        .font(Font.system(size:22, weight: .regular))
+                    Spacer()
+                }
+            }
+            
+            
+            Spacer()
         }
         .padding()
+
+     
     }
 }
 
